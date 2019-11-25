157b1601f853626859c0e9eeb26b3211abba2cbe
Phil Wood <phil@pwood.net>
2019-11-25 15:31:46 +0000

    Add project files.

---
+++ SQLiteLogin/LoginForm.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class LoginForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.UsernameTextBox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.LoginButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'UsernameTextBox
+        '
+        Me.UsernameTextBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.UsernameTextBox.Location = New System.Drawing.Point(164, 63)
+        Me.UsernameTextBox.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.UsernameTextBox.Name = "UsernameTextBox"
+        Me.UsernameTextBox.Size = New System.Drawing.Size(237, 26)
+        Me.UsernameTextBox.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(74, 66)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(83, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Username"
+        '
+        'LoginButton
+        '
+        Me.LoginButton.Location = New System.Drawing.Point(302, 194)
+        Me.LoginButton.Name = "LoginButton"
+        Me.LoginButton.Size = New System.Drawing.Size(99, 31)
+        Me.LoginButton.TabIndex = 2
+        Me.LoginButton.Text = "Login"
+        Me.LoginButton.UseVisualStyleBackColor = True
+        '
+        'LoginForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(468, 268)
+        Me.Controls.Add(Me.LoginButton)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.UsernameTextBox)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "LoginForm"
+        Me.Text = "Login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents UsernameTextBox As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents LoginButton As Button
+End Class
---
+++ SQLiteLogin/LoginForm.vb
@@ -0,0 +1,36 @@
+﻿Imports System.Data.Sqlite
+
+
+Public Class LoginForm
+
+    Private Sub LoginButton_Click(sender As Object, e As EventArgs) Handles LoginButton.Click
+
+        Dim dbConn As SQLiteConnection
+        Dim dbCmd As SQLiteCommand
+        Dim dbReader As SQLiteDataReader
+        Dim SQL As String
+        Dim Username As String
+
+        Username = UsernameTextBox.Text
+        SQL = String.Format("SELECT * from user where username = '{0}'", Username)
+
+        dbConn = New SQLiteConnection("Data Source=philtest.db;Version=3;")
+        dbConn.Open()
+
+        dbCmd = dbConn.CreateCommand
+        dbCmd.CommandText = SQL
+
+        ' execute the SQL command
+        dbReader = dbCmd.ExecuteReader()
+
+        If dbReader.Read() Then
+            MsgBox("User " & Username & " exists")
+
+        Else
+            MsgBox("User " & Username & " NOT found")
+        End If
+
+
+    End Sub
+
+End Class
